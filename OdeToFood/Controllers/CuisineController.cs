88dc949abd3d6c5ed969b512126b27931c3d2cdc
Erik Vilumaa <evilumaa@gmail.com>
2022-06-20 12:16:09 +0900

    Add post and get to cuisine controller

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -4,9 +4,16 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
+        [HttpPost]
         public IActionResult Search(string name = "french")
         {
-            return RedirectToAction("Index", "Home", new { name = name });
+            return Content("First: " + name);
+        }
+        
+        [HttpGet]
+        public IActionResult Search(string name, bool notused)
+        {
+            return Content("Search");
         }
     }
 }
