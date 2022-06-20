7708bf54240512670ba4b970391c994301330cfc
Erik Vilumaa <evilumaa@gmail.com>
2022-06-20 12:31:37 +0900

    Add an action logger.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.filter;
 
 namespace OdeToFood.Controllers
 {
+    [Log]
     public class CuisineController : Controller
     {
         [HttpPost]
