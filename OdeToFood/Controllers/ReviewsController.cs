7839424990cf1a7f52f92b27542b1ced0d3d9331
Erik Vilumaa <evilumaa@gmail.com>
2022-06-20 13:23:50 +0900

    add review editing

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OdeToFood.Data;
 using OdeToFood.Models;
 
@@ -37,6 +38,26 @@ namespace OdeToFood.Controllers
             if (ModelState.IsValid)
             {
                 _context.Reviews.Add(review);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index), new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _context.Reviews.Find(id);
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(review).State = EntityState.Modified;
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index), new { id = review.RestaurantId });
             }
